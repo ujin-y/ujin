72c016df6e298a1b21ee50f362e6f7bb22696d42
ujin <yangyj9412@gmail.com>
2024-08-14 12:47:20 +0900

    add : 5강 create react app

---
+++ react-app/react-app/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Button from "./Button"
+import styles from "./App.module.css"
+
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>welcome back!</h1>
+      <Button text={"Continue"}/>
+    </div>
+  );
+}
+
+export default App;
