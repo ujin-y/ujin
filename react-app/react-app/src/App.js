fd52946856512a5a0775f065e35c161e3183f4a9
ujin <yangyj9412@gmail.com>
2024-08-25 21:12:22 +0900

    update : Route path 수정

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -13,10 +13,10 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/movie/:id">
+        <Route  path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />}>
           <Detail />
         </Route>
-        <Route path="/">
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}>
           <Home />
         </Route>
       </Switch>
